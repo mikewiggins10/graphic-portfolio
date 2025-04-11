3bce9d695bab5d6a9399ed58b03ae35c7580e588
mikewiggins10 <mike.wiggins@acu.edu>
2025-04-11 11:39:44 -0500

    Add files via upload

---
+++ graphic-portfolio/about.php
@@ -0,0 +1,9 @@
+<?php include 'header.php'; ?>
+<main>
+  <section class="about">
+    <h1>About</h1>
+    <p>This is a placeholder for your bio. You can talk about your design philosophy, education, and areas of interest here.</p>
+    <img src="images/hero.jpg" alt="Daniel Mejia Headshot" style="max-width:300px;">
+  </section>
+</main>
+<?php include 'footer.php'; ?>
---
+++ graphic-portfolio/project.php
@@ -0,0 +1,13 @@
+<?php include 'header.php'; ?>
+<main>
+  <section class="project-detail">
+    <h1>Macaw Farms</h1>
+    <p>This project showcases branding, packaging, and environmental graphics for Macaw Farms.</p>
+    <div class="project-images">
+      <img src="images/macaw-1.jpg" alt="Macaw Farms Image 1">
+      <img src="images/macaw-2.jpg" alt="Macaw Farms Image 2">
+      <img src="images/macaw-3.jpg" alt="Macaw Farms Image 3">
+    </div>
+  </section>
+</main>
+<?php include 'footer.php'; ?>
